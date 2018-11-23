19497f669288904198b91d70a194908427ce6c2b
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2018-11-23 17:17:38 -0600

    adjustment

--- php/contact.php
+++ php/contact.php
@@ -73,7 +73,7 @@ $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
 
-if(mail($address, $e_subject, $msg, $headers)) {
+if(mail("beyzakilickolakurek@gmail.com", $e_subject, $msg, $headers)) {
 
 	// Email has sent successfully, echo a success page.
 
