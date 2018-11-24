10d2686265633d987a23d68d2026414555b01cae
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2018-11-23 20:04:40 -0600

    r

--- php/contact.php
+++ php/contact.php
@@ -72,16 +72,16 @@ $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
-mail("beyzakilickolakurek@gmail.com", $e_subject, $msg, $headers)
-// if(mail("beyzakilickolakurek@gmail.com", $e_subject, $msg, $headers)) {
-//
-// 	// Email has sent successfully, echo a success page.
-//
-// 	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
-//
-// } else {
-//
-// 	echo '<p class="error">Sorry, your message is not delivered successfully. Please contact us through email.</p>';
-//
-// }
+// mail("beyzakilickolakurek@gmail.com", $e_subject, $msg, $headers)
+if(mail("beyzakilickolakurek@gmail.com", $e_subject, $msg, $headers)) {
+
+	// Email has sent successfully, echo a success page.
+
+	echo "<p>Thank you <strong>$name</strong>, your message has been submitted to us.</p>";
+
+} else {
+
+	echo '<p class="error">Sorry, your message is not delivered successfully. Please contact us through email.</p>';
+
+}
 // ?>
