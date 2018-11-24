3e6016834fe6fd0863548849c346da92ac3a9507
beyzakilickol <43250495+beyzakilickol@users.noreply.github.com>
2018-11-23 22:57:18 -0600

    r

--- php/contact.php
+++ php/contact.php
@@ -72,7 +72,7 @@ $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
-if(mail("emreyeprem@gmail.com", $e_subject, $msg, $headers)) {
+if(mail("beyzakilickolakurek@gmail.com", $e_subject, $msg, $headers)) {
 
 	// Email has sent successfully, echo a success page.
 
